3fb738c4b63a7987b553bd92adcff8f45cb84932
Aziz ALKAN <azzlkn@gmail.com>
2024-08-15 15:56:44 +0300

    feat: create my new website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Instagram, Twitter, Youtube, Linkedin } from "lucide-react"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Aziz Alkan</h1>
+      <p className="text-balance text-muted-foreground">
+        Transforming ideas into successful ventures.
+      </p>
+      <div className="flex gap-2 mt-4">
+        <Link href="https://instagram.com/azzlkn" target="_blank">
+          <Button>
+            <Instagram className="mr-2 h-4 w-4" /> @azzlkn
+          </Button>
+        </Link>
+        <Link href="https://x.com/azzlkn" target="_blank">
+          <Button>
+            <Twitter className="mr-2 h-4 w-4" /> @azzlkn
+          </Button>
+        </Link>
+        <Link href="https://linkedin.com/in/azzlkn" target="_blank">
+          <Button>
+            <Linkedin className="mr-2 h-4 w-4" /> @azzlkn
+          </Button>
+        </Link>
+        <Link href="https://youtube.com/@azzlkn" target="_blank">
+          <Button>
+            <Youtube className="mr-2 h-4 w-4" /> @azzlkn
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-8 grid gap-4 w-full">
+        <div className="flex items-center mx-3 gap-4 border border-muted-foreground rounded-lg p-4 ">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/proses.png" alt="Avatar" />
+            <AvatarFallback>PA</AvatarFallback>
+          </Avatar>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium leading-none">
+              Proses Ajans
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Firmaların dijitalleşerek kazançlarını arttıran dijital danışmanlık ajansı.
+            </p>
+          </div>
+          <div className="ml-auto font-medium">
+            <Link href="https://prosesajans.com" target="_blank">
+              <Button>İncele</Button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-center mx-3 gap-4 border border-muted-foreground rounded-lg p-4 ">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/dada.png" alt="Avatar" />
+            <AvatarFallback>DY</AvatarFallback>
+          </Avatar>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium leading-none">
+              Dada Yazılım
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Fikirleri 4 hafta içerisinde mobil uygulama haline getiren yazılım ajansı.
+            </p>
+          </div>
+          <div className="ml-auto font-medium">
+            <Link href="https://dadayazilim.com" target="_blank">
+              <Button>İncele</Button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex items-center mx-3 gap-4 border border-muted-foreground rounded-lg p-4 ">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/taym.png" alt="Avatar" />
+            <AvatarFallback>TS</AvatarFallback>
+          </Avatar>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium leading-none">
+              Taym Studio
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Yeni nesil ürün ve içerik çekim stüdyosu.
+            </p>
+          </div>
+          <div className="ml-auto font-medium">
+            <Link href="https://tally.so/r/mYroOB" target="_blank">
+              <Button>Form Doldur</Button>
+            </Link>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="mt-8">
+        <p>&copy; {new Date().getFullYear()} Aziz Alkan</p>
+      </footer>
+    </div>
   );
 }
